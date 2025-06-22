42a6e2d46ac0e2ee946558ed625f9e1cceb8f344
softymatcha <softymatcha@gmail.com>
2025-06-22 08:27:59 +0800

    Update contents

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2025,
+                month: 11,
+                day: 11,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
@@ -130,7 +130,7 @@
                 },
                 plural: true,
                 inline: false,
-                enableUtc: true,
+                enableUtc: false,
                 onEnd: function () {
                     return;
                 },
